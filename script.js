50c1d3511a851bcfe2dfd8263618e4fbf3b5f627
Yi Feng Zhang <yifengz246@gmail.com>
2024-07-31 22:07:29 +0000

    Duck and lemonade

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-let noun = "dog";
+let noun = "Duck";
 let number = "8";
 let calculatedNumber = number * 2;
 let isSunny = true;
-let userInput = prompt("Enter an adjective to describe the day:");
-let story = `Once upon a time, a ${userInput} ${noun} decided to go for a walk. It was a ${isSunny ? "sunny" : "rainy"} day with a temperature of ${calculatedNumber} degrees. The cat did ${number} different activities throughout the day.`;
+let userInput = prompt("Enter emotion you feel:");
+let story = `Once upon a time, a ${userInput} ${noun} decided to go for a walk. It was a ${isSunny ? "sunny" : "rainy"} day with ${calculatedNumber} treats and many grapes to eat. The Duck did ${number} different activities throughout the day. Including annoying a certain lemonade stand.`;
 console.log(story);
