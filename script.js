06fc7d1219f113814014317f59e0cbc706727ae6
Yi Feng Zhang <yifengz246@gmail.com>
2024-07-31 21:48:02 +0000

    change

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 let noun = "dog";
 let number = "8";
-let calculatedNumber = number * 3;
+let calculatedNumber = number * 2;
 let isSunny = true;
 let userInput = prompt("Enter an adjective to describe the day:");
-let story =  `Once upon a time, a ${userInput} ${noun} decided to go for a walk. It was a ${isSunny ? "sunny" : "rainy"} day with a temperature of ${calculatedNumber} degrees. The cat did ${number} different activities throughout the day.`;
+let story = `Once upon a time, a ${userInput} ${noun} decided to go for a walk. It was a ${isSunny ? "sunny" : "rainy"} day with a temperature of ${calculatedNumber} degrees. The cat did ${number} different activities throughout the day.`;
 console.log(story);
